81ec05c82ae3141c50ed22f2bb176276ad39b275
sguckiran <sinanguckiran@outlook.com>
2025-11-16 22:06:59 +0000

    lesson 1 deliverables

---
+++ fetch_posts.py
@@ -0,0 +1,28 @@
+import requests
+
+def fetch_posts(limit=50):
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json" # This is the url of the top stories in Hacker News for fetching purposes.
+    ids = requests.get(url).json() # This is the json response of the top stories.
+
+    cs_stories = [] # Empty list to store the CS Stories.
+
+    for pid in ids[:limit]: # Loop through the top stories and store the CS stories in the cs_stories list.
+        item = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{pid}.json").json()
+        if item and "title" in item:
+            title = item["title"].lower()
+
+            if "research" in title or "ai" in title or "computer" in title or "machine" in title or "learning" in title or "deep" in title: # This filters the posts by having keywords.
+                cs_stories.append(item)
+
+    return cs_stories
+
+# This block runs only if the file is executed directly (not imported as a module)
+if __name__ == "__main__":
+
+    # Fetch the top 100 stories and filter them for CS-related content
+    stories = fetch_posts(100)
+
+    # Print the title of each CS-related story
+    for s in stories:
+        print(s["title"])
+
